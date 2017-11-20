937465acbf105b204dd1597d0bd89f8f4454372d
Wanich Keatkajonjumroen <toowanich@gmail.com>
2017-11-20 23:47:02 +0700

    Menu
    Updated menu to reflect addition of booking

--- menu.php
+++ menu.php
@@ -1,16 +1,15 @@
 <div class= "container">
   <nav class= "nav">
     <ul>
-      <li><a href="index.php#home">HOME</a></li>
-      <li><a href="index.php#news">NEWS</a></li>
-      <li><a href="index.php#models">MODELS</a></li>
-      <li><a href="index.php#contact">CONTACT US</a></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="index.php">NEWS</a></li>
+      <li><a href="index.php">MODELS</a></li>
+      <li><a href="Booking.php">BOOKING</a></li>
+      <li><a href="index.php">CONTACT US</a></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
         <li><?php if(isset($_SESSION["user"])){
           echo "LOG OUT";
         }else{
