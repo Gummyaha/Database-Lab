d40291553dc46e2c4662e8c686a5a31e24987ca9
Wanich Keatkajonjumroen <toowanich@gmail.com>
2017-11-26 02:26:25 +0700

    Login
    - Changed login to work
    - references both tables

--- Index.php
+++ Index.php
@@ -1,37 +1,34 @@
 <?php
-  session_start();
+session_start();
 ?>
 <html>
 
 <link rel="stylesheet" href="main.css"/>
-  <head>
-    <title> THE CAR </title>
-  </head>
+<head>
+  <title> THE CAR </title>
+</head>
 
-  <header>
-<!--MENU Bar-->
-<<<<<<< HEAD
-=======
-      <?php
-      require_once('menu.php');
-      ?>
->>>>>>> master
-    </header>
+<header>
+  <?php
+  require_once('menu.php');
+  ?>
+
+</header>
 
 
 
 
-  <body>
+<body>
 
 
   <img src="Car2.png" id="home">
 
-<!-- NEWS -->
-    <div class="header-title-news" id="news">
-      <img style="float: right; width:85%"; src="News-Header_fix.png">
-    </div>
+  <!-- NEWS -->
+  <div class="header-title-news" id="news">
+    <img style="float: right; width:85%"; src="News-Header_fix.png">
+  </div>
 
- <center>
+  <center>
     <table style="width:80%; position: center;">
       <tr>
         <td class="pol-col">
@@ -41,135 +38,135 @@
               <div class= "middle"><a href="News.html">
                 <div class="text">READ</div></a>
               </div>
-            <div class="banner-container">
-              <p> <b>MAZDA</b> promises evolution of RX-Vision 2017 concept Tokyo Motor Show </p>
-            </div>
-          </div>
-        </td>
-        <td class="pol-col">
-          <div class="polaroid">
-            <a href="News2.html"><img src="img_News1.png" class= "image" style="width:100%"></a>
-            <a href="News2.html">
-              <div class= "middle"><a href="News2.html">
-                <div class="text"> READ</div></a>
+              <div class="banner-container">
+                <p> <b>MAZDA</b> promises evolution of RX-Vision 2017 concept Tokyo Motor Show </p>
               </div>
-            <div class="banner-container">
-              <p> 2019 Porsche Cayenne, 2018 Mercedes-Benz S-Class, Borgward Isabella concept </p>
             </div>
-        </td>
-        <td class="pol-col">
-          <div class="polaroid">
-            <a href="News3.html"><img src="img_News33.png" class= "image" style="width:100%"></a>
-              <a href="News3.html">
-              <div class= "middle"><a href="News3.html">
-                <div class="text"> READ</div></a>
+          </td>
+          <td class="pol-col">
+            <div class="polaroid">
+              <a href="News2.html"><img src="img_News1.png" class= "image" style="width:100%"></a>
+              <a href="News2.html">
+                <div class= "middle"><a href="News2.html">
+                  <div class="text"> READ</div></a>
+                </div>
+                <div class="banner-container">
+                  <p> 2019 Porsche Cayenne, 2018 Mercedes-Benz S-Class, Borgward Isabella concept </p>
+                </div>
+              </td>
+              <td class="pol-col">
+                <div class="polaroid">
+                  <a href="News3.html"><img src="img_News33.png" class= "image" style="width:100%"></a>
+                  <a href="News3.html">
+                    <div class= "middle"><a href="News3.html">
+                      <div class="text"> READ</div></a>
+                    </div>
+                    <div class="banner-container">
+                      <p>First production 2018 Dodge Challenger SRT Demon rolls off assembly line</p>
+                    </div>
+
+                  </td>
+                </tr>
+              </table>
+            </center>
+
+            <center>
+              <button class="button">LOAD MORE</button>
+            </center>
+
+            <!-- MODELS -->
+            <div class="header-title-Models" id="models">
+              <img src="Models-Header.png" style="width:40%; float: left">
+            </div>
+
+            <center>
+              <table class="model-table">
+                <tr>
+                  <td>
+                    <div class="polaroid">
+                      <div class="image">
+                        <div class="model-img">
+                          <a href="modelDisplay.php"><img src="Model-test111.png" style="width: 100%;"></a>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <div class="polaroid">
+                      <div class="image">
+                        <div class="model-img">
+                          <a href="modelDisplay.php"><img src="Model-test22.png" style="width: 100%;"></a>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>
+                    <div class="polaroid">
+                      <div class="image">
+                        <div class="model-img">
+                          <a href="modelDisplay.php"><img src="Model-test33.png" style="width: 100%;"></a>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+              <button class="button">SEE MORE</button>
+            </center>
+
+
+
+
+            <!--Contact-->
+            <center>
+              <div class="header-title-Contact" id="contact">
+                <img src="ContacUst-Header2.png">
               </div>
-            <div class="banner-container">
-              <p>First production 2018 Dodge Challenger SRT Demon rolls off assembly line</p>
-            </div>
+              <br>
+              <br>
+            </center>
 
-        </td>
-      </tr>
-    </table>
-</center>
+            <!-- Google Map-->
+            <div id="googleMap" style="width:20%; height:350px; border-radius: 3px; float:left; margin: 0% 5% 3% 30%;"></div>
 
-<center>
-  <button class="button">LOAD MORE</button>
-</center>
+            <script>
+            function myMap()
+            {
+              myCenter=new google.maps.LatLng(13.75398, 100.50144);
+              var mapOptions= {
+                center:myCenter,
+                zoom:12, scrollwheel: false, draggable: false,
+                mapTypeId:google.maps.MapTypeId.ROADMAP
+              };
+              var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+              var marker = new google.maps.Marker({position: myCenter});
+              marker.setMap(map);
+            }
+            </script>
 
-<!-- MODELS -->
-  <div class="header-title-Models" id="models">
-    <img src="Models-Header.png" style="width:40%; float: left">
-  </div>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtppcpytNGMABs65egaPMz72ql-4YKD7I&callback=myMap"></script>
+            <!--End Map-->
 
-<center>
-  <table class="model-table">
-    <tr>
-      <td>
-        <div class="polaroid">
-          <div class="image">
-            <div class="model-img">
-              <a href="modelDisplay.php"><img src="Model-test111.png" style="width: 100%;"></a>
-            </div>
-          </div>
-        </div>
-      </td>
-    </tr>
+            <form>
+              <label for="name">Name</label><br>
+              <input type="text" id="name" name="name" placeholder="Your Name"><br>
+              <label for="Email">Email</label><br>
+              <input type="text" id="Email" name="Email" placeholder="Email"><br>
+              <label for="Message">Message</label><br>
+              <input type="text" id="Message" name="Message" placeholder="Your Message"><br>
+            </form>
 
-    <tr>
-      <td>
-        <div class="polaroid">
-          <div class="image">
-            <div class="model-img">
-              <a href="modelDisplay.php"><img src="Model-test22.png" style="width: 100%;"></a>
-            </div>
-          </div>
-        </div>
-      </td>
-    </tr>
+            <br>
+            <button class="button-black" type="submit">SEND MESSAGE</button>
+            <br>
+            <img src="social.png">
 
-    <tr>
-      <td>
-        <div class="polaroid">
-          <div class="image">
-            <div class="model-img">
-              <a href="modelDisplay.php"><img src="Model-test33.png" style="width: 100%;"></a>
-            </div>
-          </div>
-        </div>
-      </td>
-    </tr>
-  </table>
-  <button class="button">SEE MORE</button>
-</center>
-
-
-
-
-<!--Contact-->
-<center>
-  <div class="header-title-Contact" id="contact">
-    <img src="ContacUst-Header2.png">
-  </div>
-  <br>
-  <br>
-</center>
-
-  <!-- Google Map-->
-  <div id="googleMap" style="width:20%; height:350px; border-radius: 3px; float:left; margin: 0% 5% 3% 30%;"></div>
-
-  <script>
-    function myMap()
-    {
-      myCenter=new google.maps.LatLng(13.75398, 100.50144);
-      var mapOptions= {
-      center:myCenter,
-      zoom:12, scrollwheel: false, draggable: false,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-      };
-      var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-      var marker = new google.maps.Marker({position: myCenter});
-      marker.setMap(map);
-    }
-  </script>
-
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBtppcpytNGMABs65egaPMz72ql-4YKD7I&callback=myMap"></script>
-  <!--End Map-->
-
-  <form>
-    <label for="name">Name</label><br>
-    <input type="text" id="name" name="name" placeholder="Your Name"><br>
-    <label for="Email">Email</label><br>
-    <input type="text" id="Email" name="Email" placeholder="Email"><br>
-    <label for="Message">Message</label><br>
-    <input type="text" id="Message" name="Message" placeholder="Your Message"><br>
-  </form>
-
-<br>
-  <button class="button-black" type="submit">SEND MESSAGE</button>
-<br>
-  <img src="social.png">
-
-<img src="footer.png">
-  </body>
-</html>
+            <img src="footer.png">
+          </body>
+          </html>
--- loginCheck.php
+++ loginCheck.php
@@ -1,5 +1,45 @@
 <?php
-if(isset($_SESSION['user'])){
-
+require_once('connect.php');
+session_start();
+if(isset($_POST["user"]) and isset($_POST['password'])){
+  $q = 'SELECT customerID, disabled, password, fName FROM customer where username = "'.$_POST['user'].'"';
+  $results = $mysqli-> query($q);
+  if($results -> num_rows == 0){
+    $q2 = 'SELECT staffID, disabled, password, type, fName FROM staff where username = "'.$_POST['user'].'"';
+    $results = $mysqli-> query($q2);
+    if($results -> num_rows == 0){
+      $notFound = 1;
+    }else{
+      $row = $results->fetch_array();
+      if($row['password'] == $_POST['password']){
+        if($row['disabled'] == 0){
+          $_SESSION['user'] = $row['fName'];
+          $_SESSION['ID'] = $row['staffID'];
+          $_SESSION['role'] = $row['type'];
+        }else{
+          $_SESSION['logError'] = 'disabled';
+        }
+      }else{
+          $_SESSION['logError'] = 'wrong';
+      }
+    }
+  }else{
+    $row = $results->fetch_array();
+    if($row['password'] == $_POST['password']){
+      if($row['disabled'] == 0){
+        $_SESSION['user'] = $row['fName'];
+        $_SESSION['ID'] = $row['staffID'];
+        $_SESSION['role'] = 0;
+      }else{
+          $_SESSION['logError'] = 'disabled';
+      }
+    }else{
+        $_SESSION['logError'] = 'wrong';
+    }
+  }
 }
+if(isset($_POST["logOut"])){
+  session_unset();
+}
+header('Location:login.php');
 ?>
