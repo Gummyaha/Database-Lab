66f4b5a8251949b1ff1b169c11e6b623a12cebbd
Wanich Keatkajonjumroen <toowanich@gmail.com>
2017-11-27 17:28:33 +0700

    Models
    - Information
    - image

--- Model.php
+++ Model.php
@@ -16,10 +16,23 @@
     require_once('menu.php');
     ?>
   </header>
-
+<?php
+if(isset($_GET['model'])){
+  $q='SELECT * FROM product,brands where manufacturer = brandID and productID = '.$_GET['model'];
+  $results = $mysqli-> query($q);
+  $row = $results->fetch_array();
+}
+ ?>
   <body>
     <div class="Name-Head" style="margin: 10% 15% 5% 10%;">
-      <h1> The XXX </h1>
+      <h1>  <?php
+      if(isset($row)){
+        echo 'The '.$row['name'];
+      }else{
+        echo 'Choose a Model';
+      }
+
+      ?> </h1>
     </div>
 
 <br>
@@ -28,45 +41,91 @@
     <table style="margin: 0% 5% 2% 10%">
       <tr>
         <td class="head">Length</td>
-        <td class="detail"> xxx mm</td>
-        <td class="right-hand" rowspan="5"> Car image</td>
+        <td class="detail">
+          <?php
+          if(isset($row)){
+            echo $row['length'].' ';
+          }
+          ?>
+           mm</td>
+           <?php
+            $image = 'car-img/'.$row['productID'].'_1.jpg';
+            if(!file_exists($image)){
+              $image = 'car-img/default.jpg';
+            }
+           ?>
+        <td class="right-hand" rowspan="6"><img class = "carImage" src="<?php echo $image;  ?>"></td>
       </tr>
       <tr>
         <td class="head">Width</td>
-        <td class="detail"> xxx mm</td>
+        <td class="detail">
+          <?php
+          if(isset($row)){
+            echo $row['width'].' ';
+          }
+          ?>
+            mm</td>
       </tr>
       <tr>
         <td class="head">Height</td>
-        <td class="detail"> xxx mm</td>
+        <td class="detail">
+          <?php
+          if(isset($row)){
+            echo $row['height'].' ';
+          }
+          ?>
+            mm</td>
       </tr>
       <tr>
         <td class="head">Max speed</td>
-        <td class="detail"> xx km/h </td>
+        <td class="detail">
+          <?php
+          if(isset($row)){
+            echo $row['maxSpeed'].' ';
+          }
+          ?>
+            km/h </td>
       </tr>
       <tr>
         <td class="head">Acceleration (0 to 60)</td>
-        <td class="detail"> xx-xx Seconds</td>
-        <td class="right-hand" style="margin-left: 15%;">
-          <button id="silver" class="colorbutton">  </button>
-          <button id="black" class="colorbutton">  </button>
-          <button id="white" class="colorbutton">  </button>
-        </td>
+        <td class="detail">
+          <?php
+          if(isset($row)){
+            echo $row['acceleration'].' ';
+          }
+          ?>
+            Seconds</td>
+
+      </tr>
+      <tr>
+        <td class="head">Manufacturer</td>
+        <td class="detail">
+          <?php
+          if(isset($row)){
+            echo $row['brandName'].' ';
+          }
+          ?>
+            </td>
       </tr>
 
     </table>
 
 
-    <p style="margin-left: 10%;">Price: XX,XXX,XXX Baht</p>
+    <p style="margin-left: 10%;">Price: <?php
+    if(isset($row)){
+      echo $row['price'].' ';
+    }
+    ?> Baht</p>
     <br>
 
     <center>
-      <button class="button">Booking</button>
+      <button class="button">Test Drive</button>
       <br>
       <br>
       <br>
 
       <h3>COMMENT</h3>
-      <textarea style="color: #7c7c7c; margin:0% 0% 3% 0%;" rows="6" cols="50">comment here...</textarea>
+      <textarea style="color: #7c7c7c; margin:0% 0% 3% 0%;" rows="6" cols="50" maxlength = "200" placeholder="Comment here"></textarea>
       <br>
       <button class="button" style="margin:0% 0% 0% 20%;">Comment</button>
 
